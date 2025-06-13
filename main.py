0c4dc3eca9b391a1eec2d38a469f1d059349796c
Gomez Eduardo MNR TDPRB <eduardo.gomez2@zf.com>
2025-06-13 14:14:22 -0600

    ch1-l5

--- main.py
+++ main.py
@@ -1,20 +1,53 @@
 import os
+import sys
 from dotenv import load_dotenv
 
 from google import genai
-
+import argparse as ap
 
 load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
 
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
-)
+def setup_agent() -> genai.Client:
+    """Setups the ai agent
 
-print(response.text)
+    Returns:
+        Client
+    """
+    api_key = os.environ.get("GEMINI_API_KEY")
+    return genai.Client(api_key=api_key)
 
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+def setup_parser():
+    # Setup Parser
+    parser = ap.ArgumentParser(
+        description="This is ai agent that takes a string and returns the response"
+    )
+    parser.add_argument("contents", type=str, help="Input string to send to the agent")
+
+    if len(sys.argv) < 2:
+        print("Error: Missing required argument <input_string>.", file=sys.stderr)
+        sys.exit(1)
+
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+
+    client = setup_agent()
+    args = setup_parser()
+
+    contents: str = args.contents
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=contents,
+    )
+
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
