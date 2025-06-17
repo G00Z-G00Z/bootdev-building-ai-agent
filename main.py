31932742b77c5fb016673acabb8764af24817211
Gomez Eduardo MNR TDPRB <eduardo.gomez2@zf.com>
2025-06-17 07:52:30 -0600

    ch1-l6

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
 import sys
+import types
 from dotenv import load_dotenv
 
 from google import genai
 import argparse as ap
+
+from google.genai import types
 
 load_dotenv()
 
@@ -21,9 +24,12 @@ def setup_agent() -> genai.Client:
 def setup_parser():
     # Setup Parser
     parser = ap.ArgumentParser(
-        description="This is ai agent that takes a string and returns the response"
+        description="This is an AI agent that takes a string and returns the response"
     )
     parser.add_argument("contents", type=str, help="Input string to send to the agent")
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="Display extra information"
+    )
 
     if len(sys.argv) < 2:
         print("Error: Missing required argument <input_string>.", file=sys.stderr)
@@ -40,13 +46,23 @@ def main():
     args = setup_parser()
 
     contents: str = args.contents
+    verbose: bool = args.verbose
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=contents)]),
+    ]
 
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=contents,
+        contents=messages,
     )
 
-    print(response.text)
+    if verbose:
+        print(f"User prompt: {response.text}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    else:
+        print(response.text)
 
 
 if __name__ == "__main__":
